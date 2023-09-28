384033d3ef84e1e264a685ba09a55db1370d7d27
chehin <chehinjuan@gmail.com>
2023-09-28 20:20:58 -0300

    modify env prod

--- estetica/src/environments/environment.prod.ts
+++ estetica/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 export const environment = {
   production: true,
   base_url: '',
-  URL_SERVICIOS: 'https://lebron.chehin.online:3000/api',
-  ruta_img_productos: 'https://lebron.chehin.online:3000/uploads/images/productos/',
-  ruta_img_marcas: 'https://lebron.chehin.online:3000/uploads/images/marcas/'
+  URL_SERVICIOS: 'https://estetica.chehin.online:3000/api',
+  ruta_img_productos: 'https://estetica.chehin.online:3000/uploads/images/productos/',
+  ruta_img_marcas: 'https://estetica.chehin.online:3000/uploads/images/marcas/'
 };
