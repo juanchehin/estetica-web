5fb2e70c77af449fa356bd697aec3a4546dd8df0
chehin <chehinjuan@gmail.com>
2024-01-09 19:51:19 -0300

    modify config pool database

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -4,7 +4,13 @@ var database = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 };
 
 const pool = mysql.createPool(database);
